6cb22340ced033977f69c4b049a776a0c8c73819
Jack Hales <halesyy@gmail.com>
2024-01-23 01:30:18 +1100

    Brute forcing primes

--- components/projects/BrutePrimes.js
+++ components/projects/BrutePrimes.js
@@ -11,7 +11,53 @@ let BrutePrimes = (props) => {
          </p>
 
          <p>
-            <strong>The advantage:</strong>
+            <strong className="underline">The advantage:</strong> The mathematics I was able
+            to learn, was learnt in my "soul". Statistics, probability, percentages, rates of change,
+            multivariate analysis, were all built up on my own from a programming perspective.
+         </p>
+
+         <p>
+            <strong className="underline">The disadvantage:</strong> Many areas of mathematics
+            I now find inspiring and of great use were not accessible in my earlier programming
+            projects.
+         </p>
+
+         <p>
+            My <a className="text-blue-600 underline" href="https://github.com/halesyy/brute-primes" target="_bruteGit">"brute-forcing primes"</a> project is an 
+            example of this. I also wrote an <a className="text-blue-600 underline" href="https://jackhales.blogspot.com/2022/01/prime-number-research.html" target="_primeBlog">article</a> about it. 
+         </p>
+
+         <p>
+            This project involves brute-forcing the creation of equations. It would randomly combine 
+            the mathematical primitive characters (like sin, cos, +, -, (), etc), then attempt to 
+            convert that into a functioning callable function.
+         </p>
+         
+         <p>
+            From there, it would input x values (in varying forms), and compare the difference to the
+            prime number series. Different error calculations would be used to determine fitness of
+            equations.
+         </p>
+
+         <p>
+            This worked well! And created cool equations like:
+            <ul className="ml-10 mt-5">
+               <li>f(x) = x log(x, 7)</li>
+            </ul>
+         </p>
+
+         <p>
+            Where x is iterated by 2. Or the same as (x * 2) substituted.
+         </p>
+
+         <p>
+            The problem was that the difference in the prime series and the plotted would always
+            start to receive incremental errors. I'd then plot the difference as a chart, and get a
+            positive exponential growth chart. That was as far as I got - and life got in the way.
+         </p>
+
+         <p>
+            More to come... When I have more time!
          </p>
       </div>
    )
